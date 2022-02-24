7f4ab279d6d37ab5aef7c5792aed50994e4ee5d8
Renato Padilla <renato.padilla@epn.edu.ec>
2022-02-24 00:48:57 -0500

    LighCamera

--- ProyectoCG/Main.cpp
+++ ProyectoCG/Main.cpp
@@ -31,6 +31,7 @@ int main()
 
 
 	Shader shaderProgram("default.vert", "default.frag");
+	Shader lightingShader("lighting.vs", "lighting.fs");
 
 	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
 	glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
@@ -56,6 +57,9 @@ int main()
 	std::string modelPath3 = "Resources/3december_2021_day_18_astronaut/scene.gltf";
 	Model model3((modelPath3).c_str());
 
+	lightingShader.use(); 
+    lightingShader.setInt("material.diffuse", 0);
+    lightingShader.setInt("material.specular", 1);
 
 	float angle = glfwGetTime();
 
@@ -72,6 +76,39 @@ int main()
 
 		camera.Inputs(window);
 		camera.updateMatrix(0.1f, 1000.0f);
+		
+		lightingShader.use();
+	 
+		lightingShader.setVec3("light.position", camera.Position);
+		lightingShader.setVec3("light.direction", camera.Front);
+		lightingShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
+		lightingShader.setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
+		lightingShader.setVec3("viewPos", camera.Position);
+		
+		lightingShader.setVec3("light.ambient", 0.1f, 0.1f, 0.1f);
+		// we configure the diffuse intensity slightly higher; the right lighting conditions differ with each lighting method and environment.
+		// each environment and lighting type requires some tweaking to get the best out of your environment.	 
+		 lightingShader.setVec3("light.diffuse", 0.8f, 0.8f, 0.8f);
+		 lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+		 
+		 //Exercise 15 Task 2
+		 lightingShader.setFloat("light.constant", 1.0f);
+		 lightingShader.setFloat("light.linear", 0.09f);
+		 lightingShader.setFloat("light.quadratic", 0.032f);
+	 
+
+		 // material properties
+		 lightingShader.setFloat("material.shininess", 32.0f);
+		 
+		 // view/projection transformations
+		 glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		 glm::mat4 view = camera.GetViewMatrix();
+		 lightingShader.setMat4("projection", projection);
+		 lightingShader.setMat4("view", view);
+
+		 // world transformation
+		 glm::mat4 model = glm::mat4(1.0f);
+		 lightingShader.setMat4("model", model);
 
 		model.Draw(shaderProgram, camera, glm::vec3(0.0f,0.0f,0.0f), glm::vec3(angle, -2.5f, 0.0f), 0.5f);
 
